7b26e413099b3de7e6e257ca128a848d5387a5af
ZanMervic <zan.mervic@gmail.com>
2022-11-07 18:25:33 +0100

    Completed task 1 and 3, partly 2 -> mutation works, starting population in progress

---
+++ assignment.r
@@ -0,0 +1,364 @@
+library(GA)
+
+map_reading <- function(maze){
+  #In mazer.r all mazes are square shaped so we might only need the rows parameter
+  rows = length(maze)
+  columns = nchar(maze[1])
+  #We initialize an empty matrix of the correct dimensions
+  maze_matrix = matrix(0, nrow = rows, ncol = columns)
+  #We fill the matrix with correct values
+  for (i in c(1:rows)) {
+    maze_matrix[i,] <- unlist(strsplit(maze[i], ""))
+  }
+  return (maze_matrix)
+}
+
+#FOR TESTING ONLY
+test_solution <- function(path){
+  maze_matrix = map_reading(my_maze)
+  start <- as.vector(t(which(maze_matrix == 'S', arr.ind = TRUE)))
+  exit <- as.vector(t(which(maze_matrix == 'E', arr.ind = TRUE)))
+  current = start
+  penalties <- 0.0
+  treasures <- 0
+  for (i in seq(1,length(path), 2)) {
+    move <- path[i : (i+1)]
+    if(all(move == c(0,0))){ #UP
+      current <- current - c(1,0)
+    } else if(all(move == c(0,1))){ #RIGHT
+      current <- current + c(0,1)
+    } else if(all(move == c(1,0))){ #DOWN
+      current <- current + c(1,0)
+    } else if(all(move == c(1,1))){ #LEFT
+      current <- current - c(0,1)
+    } else{
+      print("Error")
+    }
+    if(!all(current > 0) || !all(current < dim(maze_matrix)) || maze_matrix[current[1], current[2]] == '#'){
+      print("HIT A WALL")
+      return(list(maze=maze_matrix, position=current, penalties=penalties, treasures=treasures))
+    }
+    if(maze_matrix[current[1], current[2]] == 'o' || maze_matrix[current[1], current[2]] == 'S'){
+      penalties <- penalties + 10
+    }else{
+      penalties <- penalties - 5
+    }
+    if(maze_matrix[current[1], current[2]] == 'T'){
+      treasures <- treasures + 1
+    }
+    if(maze_matrix[current[1], current[2]] == 'E'){
+      print("MADE IT TO THE END")
+      return(list(maze=maze_matrix, position=current, penalties=penalties, treasures=treasures))
+    }
+    maze_matrix[current[1], current[2]] <- "o"
+  }
+  print("RAN OUT OF MOVES")
+  return(list(maze=maze_matrix, position=current, penalties=penalties, treasures=treasures))
+  
+}
+
+
+
+#Function that gets a maze, move and current position and changes the move and position to a valid one
+#We need to make sure we only made 1 invalid move !!! 
+#And that we are not out of bounds
+'generate_valid_move <- function(maze_matrix, prev_move, current){
+  tryCatch(expr = {
+    #If the function input is invalid
+    if(any(current < 1) || any(current > dim(maze_matrix)[2])){
+      return(matrix(c(current, prev_move), ncol = 2, byrow = TRUE))
+    }
+    
+    #We change the position to the one before the move
+    if(all(prev_move == c(0,0))){ #UP -> go down
+      current <- current + c(1,0)
+    } else if(all(prev_move == c(0,1))){ #RIGHT -> go left
+      current <- current - c(0,1)
+    } else if(all(prev_move == c(1,0))){ #DOWN -> go up
+      current <- current - c(1,0)
+    } else if(all(prev_move == c(1,1))){ #LEFT -> go right
+      current <- current + c(0,1)
+    } else{
+      print("Error in change_to_valid")
+    }
+    
+    
+    
+    
+    #List of possible moves
+    possible_moves <- matrix(c(0,0,0,1,1,0,1,1), ncol = 2, byrow = TRUE)
+    count = 4
+    
+    #We chech all neighboring cells for walls, and remove invalid possible moves
+    if(current[1] <= 1 || maze_matrix[current[1] - 1, current[2]] == "#"){
+      possible_moves <- possible_moves[-(count-3),]
+      count <- count - 1
+    }
+    if(current[1] >= dim(maze_matrix)[1] || maze_matrix[current[1] + 1, current[2]] == "#"){
+      possible_moves <- possible_moves[-(count-2),]
+      count <- count - 1
+    }
+    if(current[2] <= 1 || maze_matrix[current[1], current[2] - 1] == "#"){
+      possible_moves <- possible_moves[-(count-1),]
+      count <- count - 1
+    }
+    if(current[2] >= dim(maze_matrix)[2] || maze_matrix[current[1], current[2] + 1] == "#"){
+      if(count == 1){
+        #Neki se je zgodilo narobe in zavrzemo ta entity
+        return(matrix(c(current, c(2,2)), ncol = 2, byrow = TRUE))
+      }else{
+        possible_moves <- possible_moves[-count,]
+        count <- count - 1
+      }
+    }
+    
+    
+    #We select a random move from remaining possible moves
+    if(is.null(dim(possible_moves)[1])){
+      new_move <- possible_moves
+    } else{
+      new_move <- possible_moves[sample(1:dim(possible_moves)[1], 1),]
+    }
+    
+    
+    #We adjust the current position accordingly
+    if(all(new_move == c(0,0))){ #UP
+      current <- current - c(1,0)
+    } else if(all(new_move == c(0,1))){ #RIGHT
+      current <- current + c(0,1)
+    } else if(all(new_move == c(1,0))){ #DOWN
+      current <- current + c(1,0)
+    } else if(all(new_move == c(1,1))){ #LEFT
+      current <- current - c(0,1)
+    } else{
+      print("Error2 in change_to_valid")
+    }
+    
+    #Neki se je zgodilo narobe in zavrzemo ta entity
+    if(!all(current > 0) || !all(current <= dim(maze_matrix))){
+      return(matrix(c(current, c(2,2)), ncol = 2, byrow = TRUE))
+    }
+    
+    return(matrix(c(current, new_move), ncol = 2, byrow = TRUE))
+  }, error = {
+    return(matrix(c(current, c(2,2)), ncol = 2, byrow = TRUE))
+  }, warning = {
+    return(matrix(c(current, c(2,2)), ncol = 2, byrow = TRUE))
+  }
+  )
+  
+  
+  
+}'
+
+
+
+custom_population <- function(object,...){
+  #We create a starting random matrix of our population
+  my_population <- matrix(sample(0:1, my_nBits * my_popSize, replace = TRUE), nrow=my_popSize)
+  
+  #We set the minimum required steps for our entity to be valid
+  required_steps <- my_nBits / 8
+  
+  #We check how many steps are made before going into a wall, if its less than required steps we generate a new value
+  maze_matrix = map_reading(my_maze)
+  for(row in 1:2){
+    print(row)
+    path <- my_population[row,]
+    steps_made <- 0
+    valid <- FALSE
+    
+    #We count the moves with our current path and change the path if invalid
+    while(!valid){
+      current <- as.vector(t(which(maze_matrix == 'S', arr.ind = TRUE)))
+      for (i in seq(1, length(path), 2)) {
+        move <- path[i : (i+1)]
+        if(all(move == c(0,0))){ #UP
+          current <- current - c(1,0)
+        } else if(all(move == c(0,1))){ #RIGHT
+          current <- current + c(0,1)
+        } else if(all(move == c(1,0))){ #DOWN
+          current <- current + c(1,0)
+        } else if(all(move == c(1,1))){ #LEFT
+          current <- current - c(0,1)
+        } else{
+          print('Error in custom pupulation')
+        }
+        
+        #If we hit a wall we generate a different (valid) move
+        if(!all(current > 0) || !all(current < dim(maze_matrix)) || maze_matrix[current[1], current[2]] == '#'){
+          steps_made = 0
+          path <- sample(0:1, my_nBits, replace = TRUE)
+          break
+        }
+        else if(maze_matrix[current[1], current[2]] == 'E'){
+          #If we have a solution that reaches the finish we keep it
+          valid <- TRUE
+          break
+        }
+        
+        steps_made <- steps_made + 1
+        
+        #If we made enough steps we stop the iteration
+        if(steps_made >= required_steps){
+          valid <- TRUE
+          break
+        }
+      }
+    }
+    
+    my_population[row,] <- path
+    steps_made = 0
+  }
+  return(my_population)
+}
+
+
+custom_mutation <- function(object, parent)
+{
+  #Select the parent vector from the population
+  mutate <- parent <- as.vector(object@population[parent,])
+  n <- length(parent)
+  #Sample a random vector element that should be changed
+  #We make sure j is a odd number (first number of a move pair)
+  j <- sample(1:n, size = 1)
+  if (j %% 2 != 1){
+    j <- j -1
+  }
+  
+  #We now go from the first to the j-th bit and check if there is a wall hit, if there
+  #is we make a mutation on the bit responsible for the hit
+  maze_matrix = map_reading(my_maze)
+  current <- as.vector(t(which(maze_matrix == 'S', arr.ind = TRUE)))
+  move <- c(0,0)
+  for (i in seq(1, j, 2)) {
+    move <- mutate[i : (i+1)]
+    if(all(move == c(0,0))){ #UP
+      current <- current - c(1,0)
+    } else if(all(move == c(0,1))){ #RIGHT
+      current <- current + c(0,1)
+    } else if(all(move == c(1,0))){ #DOWN
+      current <- current + c(1,0)
+    } else if(all(move == c(1,1))){ #LEFT
+      current <- current - c(0,1)
+    } else{
+      print("Error in custom mutation")
+    }
+    if(!all(current > 0) || !all(current < dim(maze_matrix)) || maze_matrix[current[1], current[2]] == '#'){
+      #If we hit a wall before we get to the randomly selected "j" bit we change j to the current bit
+      #so we make the mutation where we would have hit the wall
+      j = i
+      break
+    }
+    if(maze_matrix[current[1], current[2]] == 'E'){
+      return(mutate)
+    }
+  }
+  
+  
+  #Change that element to a random move that is different than the invalid one
+  
+  "new_move <- generate_valid_move(maze_matrix, move, current)[2,]
+  if(new_move[1] == 2){
+    #Something went wrong, we use a random new move
+    new_move <- c(sample(0:1, 1),sample(0:1, 1))
+    while(all(new_move == move)){
+      new_move <- c(sample(0:1, 1),sample(0:1, 1))
+    }
+  }"
+  
+  new_move <- c(sample(0:1, 1),sample(0:1, 1))
+  while(all(new_move == move)){
+    new_move <- c(sample(0:1, 1),sample(0:1, 1))
+  }
+  
+  
+  mutate[j] <- new_move[1]
+  mutate[j + 1] <- new_move[2]
+  return(mutate)
+}
+
+
+
+fitness <- function(path){
+  #Dobimo vektor koordinat zacetne pozicije, inicializiramo ostale spremenljivke
+  maze_matrix = map_reading(my_maze)
+  start <- as.vector(t(which(maze_matrix == 'S', arr.ind = TRUE)))
+  exit <- as.vector(t(which(maze_matrix == 'E', arr.ind = TRUE)))
+  current = start
+  width = length(maze_matrix[1,])
+  penalties <- 0.0
+  
+  
+  
+  #Gremo skozi celoten vektor (po 2 bita pretvorimo v move) in spreminjamo trenutno lokacijo, ter preverjamo veljavnost koraka
+  for (i in seq(1,length(path), 2)) {
+    #We get the next move
+    move <- path[i : (i+1)]
+    #We adjust the current position accordingly
+    if(all(move == c(0,0))){ #UP"
+      current <- current - c(1,0)
+    } else if(all(move == c(0,1))){ #RIGHT
+      current <- current + c(0,1)
+    } else if(all(move == c(1,0))){ #DOWN
+      current <- current + c(1,0)
+    } else if(all(move == c(1,1))){ #LEFT
+      current <- current - c(0,1)
+    } else{
+      print("Error in fitness")
+    }
+    
+    
+    
+    
+    if(!all(current > 0) || !all(current <= dim(maze_matrix)) || maze_matrix[current[1], current[2]] == '#'){
+      #We are either out of a matrix or on a wall, dont return 0 because its better if we 
+      #hit the wall closer (and make more valid moves) to the exit so we should reward that
+      #If we add -1000 in front it will work better for some mazes but worse at other
+      return (-penalties - 2 * sqrt((current[1] - exit[1])^2 + (current[2] - exit[2])^2))
+    }
+    
+    if(maze_matrix[current[1], current[2]] == 'o' || maze_matrix[current[1], current[2]] == 'S'){
+      #If we go to an already visited spot we get a penalty
+      penalties <- penalties + 10
+    }else{
+      #If we go to a unvisited spot we get rewarded
+      penalties <- penalties - 5
+    }
+    
+    if(maze_matrix[current[1], current[2]] == 'T'){
+      #If we collect a treasure we get rewarded and the visited tiles reset
+      penalties <- penalties - 200
+      maze_matrix[maze_matrix == 'o'] <- '-'
+      maze_matrix[maze_matrix == 'S'] <- 's'
+    }
+    
+    if(maze_matrix[current[1], current[2]] == 'E'){
+      #We made it to the exit, we reward the path with the least steps
+      return (1000 + 1000/i - min(penalties, 0))
+    }
+    
+    #We mark the tile as visited, so we dont visit it again
+    maze_matrix[current[1], current[2]] <- "o"
+    
+    
+  }
+  #All moves were done and we did not make it to the exit
+  return(100/sqrt((current[1] - exit[1])^2 + (current[2] - exit[2])^2) - penalties)
+}
+
+my_maze <- maze5
+my_nBits <- ceiling((nchar(my_maze[1])^2) / 2) * 2
+my_popSize = 1000
+
+#A suitable value for nBits is (nchar(mazex[1])^2) / 2
+GA <- ga(type = "binary", fitness = fitness, nBits = my_nBits, maxiter = 1000, run = 100, popSize = my_popSize, pmutation = 0.3, mutation=custom_mutation, population = custom_population)
+
+
+summary(GA)
+GA@solution
+
+test_solution(GA@solution[1,])
+
+#Can complete mazes consistently: 1,2,3,4,5
+#Can collect all treasures: 3T,4T,(5T)
